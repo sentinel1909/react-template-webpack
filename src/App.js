603d335cc3906f0bdce910634892ee4ba7a8397a
Jeff Mitchell <jeff@foschimitchell.ca>
2022-01-29 20:27:41 -0800

    Created CSS module for styling main app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import React from "react";
 
 import Test from "./content/test.mdx";
 
+import styles from "./styles/app.module.css";
+
 function App() {
   return (
-    <div>
-      <h2>Welcome to React App</h2>
+    <main className={styles.container}>
+      <h1>Welcome to a React Template App</h1>
+      <h2>(bundling courtesy of Webpack)</h2>
       <h3>Date: {new Date().toDateString()}</h3>
       <Test />
-    </div>
+    </main>
   );
 }
 
