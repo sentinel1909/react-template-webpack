608949f75d55c7dc6b64902a93fadbf04c28c729
Jeff Mitchell <sentinel1909@protonmail.com>
2022-05-30 22:07:13 -0700

    Update dependencies, remove css modules

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import React from "react";
 
 import Test from "./content/test.mdx";
 
-import styles from "./styles/app.module.css";
-
 function App() {
   return (
-    <main className={styles.container}>
+    <main>
       <h1>Welcome to a React Template App</h1>
       <h2>(bundling courtesy of Webpack)</h2>
       <h3>Date: {new Date().toDateString()}</h3>
